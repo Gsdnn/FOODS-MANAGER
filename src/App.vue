e9ff2776d0091e373cee1435b2fdcd616d905805
Gsdnn <58304867+Gsdnn@users.noreply.github.com>
2022-07-08 17:28:36 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from 'com/HelloWorld.vue'
+import { reactive } from 'vue';
+const ageList = reactive([2,3,4,5])
+const parenClick=(msg:string)=>{
+  console.log("msg",msg)
+  alert(msg)
+}
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld  ageList={{ageList}}  @parenClick="parenClick"/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { emit } from 'process';
+import { ref } from 'vue'
+// const emits = defineEmits(['parenClick']) 方法声明
+
+const emits =defineEmits<{
+  (e:"parenClick",data:string):void 
+}>()
+const clickEmits = ()=>{
+  emits('parenClick',"子组件传递数据")
+}
+
+const props= withDefaults(defineProps<{ 
+  msg?: string,
+  ageList:Array<number>
+
+}>(),{msg:"hello",ageList:()=>[1,2]} ) //对象或者数组写法
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h1>{{ageList}}</h1>
+  <button @click="clickEmits">button</button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
